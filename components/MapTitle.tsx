57756e92c00ea83f7fbf6f729c20a270453fc7f7
Joshua Melo <joshuamelo@gmail.com>
2022-12-06 21:19:31 -0500

    fixing stuff

--- components/MapTitle.tsx
+++ components/MapTitle.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 const MapTitle = ({ mapName, isSelected }: Props) => {
   const mapStyle = isSelected
-    ? "bg-slate-300 h-28 md:h-16 w-full mb-4 h-16 p-4 text-2xl flex items-center justify-center  uppercase text-center border-4 border-green-400 text-black font-bold"
+    ? "bg-slate-300 h-28 md:h-16 w-full mb-4  p-4 text-2xl flex items-center justify-center  uppercase text-center border-4 border-green-400 text-black font-bold"
     : " w-full mb-4 h-28 md:h-16 p-4 text-2xl flex items-center justify-center bg-black uppercase text-center text-white font-bold";
   return (
     <>
